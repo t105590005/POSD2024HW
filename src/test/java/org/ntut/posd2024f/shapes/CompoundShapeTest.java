ce423eeaae51fa05c09219f292cff36ce0a6b429
曾祥語 <112c53028@cc.ntut.edu.tw>
2024-09-30 11:34:19 +0800

    hw2_3

--- src/test/java/org/ntut/posd2024f/shapes/CompoundShapeTest.java
+++ src/test/java/org/ntut/posd2024f/shapes/CompoundShapeTest.java
@@ -1,9 +1,7 @@
 package org.ntut.posd2024f.shapes;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
