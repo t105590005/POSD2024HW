e5a4136aa579750b5713727a25560289e36fbc02
曾祥語 <112c53028@cc.ntut.edu.tw>
2024-10-09 16:27:17 +0800

    hw3 finish

--- src/main/java/org/ntut/posd2024f/shapes/FindShapeVisitor.java
+++ src/main/java/org/ntut/posd2024f/shapes/FindShapeVisitor.java
@@ -40,7 +40,6 @@ public class FindShapeVisitor implements Visitor<List<Shape>> {
         if (predicate.test(convexPolygon)) {
             result.add(convexPolygon);
         }
-
     }
 
     @Override
@@ -51,7 +50,9 @@ public class FindShapeVisitor implements Visitor<List<Shape>> {
         Iterator<Shape> iterator = compoundShape.iterator();
         while (iterator.hasNext()) {
             Shape shape = iterator.next();
-            shape.accept(this);
+            FindShapeVisitor findShapeVisitor = new FindShapeVisitor(predicate);
+            shape.accept(findShapeVisitor);
+            result.addAll(findShapeVisitor.getResult());
         }
     }
 
